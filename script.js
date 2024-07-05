01be2994e2905798d2e14d929a54a9f89bd7864f
aaronmj2003 <152460913+aaronmj2003@users.noreply.github.com>
2024-07-05 08:14:03 -0700

    text + bar changes

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
         img.addEventListener('click', () => {
             if (img.classList.contains('expanded')) {
                 img.classList.remove('expanded');
+                img.nextElementSibling.style.opacity = '0';
             } else {
                 // Remove the expanded class from all images before adding to the clicked one
-                expandableImages.forEach(i => i.classList.remove('expanded'));
+                expandableImages.forEach(i => {
+                    i.classList.remove('expanded');
+                    i.nextElementSibling.style.opacity = '0';
+                });
                 img.classList.add('expanded');
+                img.nextElementSibling.style.opacity = '1';
             }
         });
     });
 });
+
