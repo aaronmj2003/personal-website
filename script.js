0f7bb93a724795c309bb6e801b55a57afae1d7d3
Aaron Jones <aaronj23@uw.edu>
2024-10-21 23:32:52 -0700

    nav bar changes, added turbolearn to html, changed resume

--- script.js
+++ script.js
@@ -19,3 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+window.addEventListener('scroll', function() {
+    var docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    var scrolled = (window.scrollY / docHeight) * 100;
+    document.getElementById('progress-bar').style.width = scrolled + "%";
+});
